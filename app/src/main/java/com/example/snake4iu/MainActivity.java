d866c3981d8056e5cbb508ea78e3b526e98d41d8
Tim <knautschka@googlemail.com>
2023-04-24 15:27:25 +0200

    Notification bei Abwesenheit + Lauffähigkeit ab Android 12
    Der Nutzer wird jetzt benachrichtigt, wenn er 24 Stunden lang die App nicht geöffnet hat (zum besseren Testen der App erscheint die Nachricht aber schon nach 5 Sekunden). Zudem wurde eine Bibliothek aktualisiert, sodass die App jetzt auch ab Android 12 und höher lauffähig ist.

--- app/src/main/java/com/example/snake4iu/MainActivity.java
+++ app/src/main/java/com/example/snake4iu/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.snake4iu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Intent intent = new Intent(this, MyIntentService.class);
+        this.startService(intent);
+    }
+
     public void startSnake(View view) {
         Intent intent = new Intent(this, SnakeActivity.class);
 
         startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/snake4iu/MyIntentService.java
@@ -0,0 +1,119 @@
+package com.example.snake4iu;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class MyIntentService extends IntentService {
+
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.example.snake4iu.action.FOO";
+    private static final String ACTION_BAZ = "com.example.snake4iu.action.BAZ";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "com.example.snake4iu.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.example.snake4iu.extra.PARAM2";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            try {
+                // Normalerweise sollten hier 24 * 60 * 60 * 1000 Millisekunden angegeben sein.
+                // Dies würde dann 24 Stunden entsprechen.
+                // Damit die App zunächst gut testbar ist, sind hier 5000 Millisekunden, also
+                // 5 Sekunden eingestellt.
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            sendNotification();
+        }
+    }
+
+    private void sendNotification() {
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.zurueck_button)
+                .setContentTitle("Längere Abwesenheit")
+                .setContentText("Du hast schon 24 Stunden lang nicht mehr gespielt. Wie wäre es mit einer Runde?")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Du hast schon 24 Stunden lang nicht mehr gespielt. Wie wäre es mit einer Runde?\n" +
+                        " (Dies ist eine Testmeldung. Es sind nur 5 Sekunden vergangen, seitdem du die App geschlossen hast.)"))
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(0, builder.build());
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
