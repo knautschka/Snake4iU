bf91a83e27c13c0ca6b661187b790a96332a9713
Tim <knautschka@googlemail.com>
2023-04-24 15:59:13 +0200

    Mail-Intent für Weiterempfehlung
    Der Mail-Intent für die Weiterempfehlung des Spiels ist jetzt eingerichtet.

--- app/src/main/java/com/example/snake4iu/MainActivity.java
+++ app/src/main/java/com/example/snake4iu/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.snake4iu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SnakeActivity.class);
 
         startActivity(intent);
+    }
+
+    public void sendMail(View view) {
+        String[] to = {"example@example.com"};
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, to);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Weiterempfehlung");
+        intent.putExtra(Intent.EXTRA_TEXT, "Spiel doch mal Snake 4 (i)U!");
+
+        startActivity(intent);
+        finish();
     }
 }
