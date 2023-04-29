7b5d77a16d95232142151d01f0163899d37bd656
Tim <knautschka@googlemail.com>
2023-04-29 20:56:30 +0200

    Nachricht bei Abwesenheit durch AlarmManager
    Die Nachricht, dass der Spieler das Spiel mal wieder spielen soll, wird nun durch einen AlarmManager ausgelöst. Zurzeit ist es zum besseren Testen so eingestellt, dass die Nachricht direkt angezeigt wird, wenn die App gestartet wird. Bei einigen Smartphones kann es aber sein, dass die Nachricht erst bis zu ein paar Minuten später angezeigt wird.

--- app/src/main/java/com/example/snake4iu/AlarmReceiver.java
+++ app/src/main/java/com/example/snake4iu/AlarmReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -13,9 +14,11 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        Toast.makeText(context, "Die Alarm-Benachrichtung wurde ausgelöst!", Toast.LENGTH_LONG).show();
+
         Intent i = new Intent(context, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_MUTABLE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "awayMessage")
                 .setSmallIcon(R.drawable.zurueck_button)
@@ -23,7 +26,7 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setContentText("Dies ist eine Testnachricht. Du sollst mal wieder spielen!!!")
                 .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
--- app/src/main/java/com/example/snake4iu/MainActivity.java
+++ app/src/main/java/com/example/snake4iu/MainActivity.java
@@ -1,54 +1,42 @@
 package com.example.snake4iu;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
-
-import com.example.snake4iu.databinding.ActivityMainBinding;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    // private ActivityMainBinding binding;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNotificationChannel();
-
         createNotificationChannelAlarm();
 
-        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setAlarm();
 
-        binding*/
-
-
-
-        /*Intent intent = new Intent(this, MyIntentService.class);
-        this.startService(intent);*/
     }
 
     private void createNotificationChannelAlarm() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "awayMessageNotificationChannel";
             String description = "Kanal für die Benachrichtigung";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel("awayMessage", name, importance);
             channel.setDescription(description);
 
@@ -57,32 +45,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setAlarm() {
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        createNotificationChannel();
-        Intent intent = new Intent(this, MyIntentService.class);
-        this.startService(intent);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (alarmManager != null && !alarmManager.canScheduleExactAlarms()) {
+                intent.setAction(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                this.startActivity(intent);
+            }
+        }
+
+        // Hier wird später die richtige Alarmzeit gesetzt (24 Stunden nach der aktuellen Uhrzeit).
+        // Zurzeit ist die App so eingestellt, dass der Alarm sofort beim Start der MainActivity
+        // losgeht. Auf einiges Smartphones (auf meinem ZTE zum Beispiel) ist es aber so, dass
+        // die Benachrichtigung verzögert gesendet wird. Das variiert je nach Hersteller.
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, 10);
+
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) {
+            alarmManager.setAlarmClock(
+                    new AlarmManager.AlarmClockInfo(Calendar.getInstance().getTimeInMillis(), pendingIntent),
+                    pendingIntent
+            );
+        } else {
+            alarmManager.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    Calendar.getInstance().getTimeInMillis(),
+                    pendingIntent
+            );
+        }
+
+        Toast.makeText(this, "Alarm wurde gesetzt!", Toast.LENGTH_LONG).show();
+    }
+
+    private void cancelAlarm() {
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+
+        if(alarmManager == null) {
+            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        }
+
+        alarmManager.cancel(pendingIntent);
     }
 
     public void startSnake(View view) {
         Intent intent = new Intent(this, SnakeActivity.class);
 
         startActivity(intent);
-    }
-
-    private void createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    "1",
-                    "App-Benachrichtigungskanal",
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 
 
